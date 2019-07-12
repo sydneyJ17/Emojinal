1995f895a6fcd083efc1a1591c0561e412e6ab04
Apple <apple@Apples-MacBook-Pro-4.local>
2019-07-12 11:53:20 -0500

    complete iteration 3

--- emojinal/ViewController.swift
+++ emojinal/ViewController.swift
@@ -9,40 +9,71 @@
 import UIKit
 
 class ViewController: UIViewController {
+  
+    let emojis = ["🍽" : "Food" , "🎡" : "Attractions"]
 
-    @IBOutlet weak var microphone: UIButton!
     
-    let emojis = ["🎤" : "Tounge Tied" , "🎸" : "guitar"]
+//BUTTONS
+    @IBOutlet weak var eatButton: UIButton!
     
     
+
+//DICTIONARY- MESSAGES FOR EACH BUTTON
+    var customMessages = [
+        "Food" : [ "Giordanos" , "Eataly" , "Purple Pig" , "Harry Caray's"] ,
+        "Attractions" : ["Riverwalk" , "The Bean" , "Navy Pier" , "Willis Tower"]
+    ]
+    
+//RANDOM MESSAGE CHOOSER
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+  
     
+//ACTIONS FOR FOOD BUTTON
     @IBAction func showMessage(sender: UIButton) {
+        let selectedEmotion = sender.titleLabel?.text
         
-           
-            let selectedEmotion = sender.titleLabel?.text
+        let random:UInt32 = arc4random_uniform(4)
+        
+        let emojiMessage = customMessages[emojis[selectedEmotion!]!]?[Int(random)]
+        
+        let alertController = UIAlertController(title: emojis[selectedEmotion!], message: emojiMessage, preferredStyle: UIAlertController.Style.alert)
+     
+        
             
-            //after button is taped, the message displays
-            //there will be multiple messages
-            
-            let alertController = UIAlertController(title: "genre", message: "tounge tied", preferredStyle: UIAlertController.Style.alert)
-            
-            alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
             
             present(alertController, animated: true, completion: nil)
         
         }
+    
+    
+//ACTIONS FOR ATTRACTION BUTTON
+    @IBAction func attractionsButton(_ sender: UIButton) {
+        let selectedEmotion = sender.titleLabel?.text
+        
+        let random: UInt32 = arc4random_uniform(4)
+        
+        let emojiMessage = customMessages[emojis[selectedEmotion!]!]?[Int(random)]
         
         
+        let alertController = UIAlertController(title: emojis[selectedEmotion!], message: emojiMessage, preferredStyle: UIAlertController.Style.alert)
+        
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        
+        present(alertController, animated: true, completion: nil)
+    
+    }
+    
+    
+
+    
+    
         
 }
-
-
-
 
 
 
